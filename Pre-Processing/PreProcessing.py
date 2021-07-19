6b7e2686465d54ced79c9e92b5daea4a72ccc35d
Tristan Bobb <Tobi.Bobb.TB@gmail.com>
2021-07-19 12:05:50 -0700

    Finished pre-processing and added some testing

--- Pre-Processing/PreProcessing.py
+++ Pre-Processing/PreProcessing.py
@@ -11,6 +11,7 @@ def rescaleImage(img, scale=0.75):
     return cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
 
 
+
 def grayscaleImage(img_Path):
     """Apply grayscale onto image and then rescale"""
     img = cv.imread(str(img_Path), cv.IMREAD_GRAYSCALE)
@@ -25,8 +26,9 @@ def grayscaleImage(img_Path):
 def blurImage(img):
     """Apply Gaussian blurring on image"""
     out = cv.GaussianBlur(img, (9, 9), 0,)
-    cv.imshow('Gaussian Blur', out)
-    cv.waitKey(0)
+
+    # cv.imshow('Gaussian Blur', out)
+    # cv.waitKey(0)
     return out
 
 
@@ -34,8 +36,9 @@ def thresholdImage(img):
     """Adaptive Gaussian Thresholding"""
     out = cv.adaptiveThreshold(
         img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-    cv.imshow('Gaussian Thresholding', out)
-    cv.waitKey(0)
+
+    # cv.imshow('Gaussian Thresholding', out)
+    # cv.waitKey(0)
     return out
 
 
@@ -44,6 +47,23 @@ def invertImage(img):
     invert = cv.bitwise_not(img, img)
     kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]], np.uint8)
     out = cv.dilate(invert, kernel)
-    cv.imshow('Inverted and Dilated Image', out)
-    cv.waitKey(0)
+    
+    # cv.imshow('Inverted and Dilated Image', out)
+    # cv.waitKey(0)
     return out
+
+def process(img_Path, blur = None ,threshold = None, inversion = None):
+    img = grayscaleImage(img_Path)
+    if blur is not None:
+        processBlur = blur(img)
+    if threshold is not None:
+        processThreshold = threshold(processBlur)
+    if inversion is not None:
+        out = inversion(processThreshold)
+    return out
+    
+    
+"""FOR TESTING"""
+
+
+# img = process('Pre-Processing/Photos/sudoku_board.jpg', blurImage, thresholdImage, invertImage) 
--- machine_learning/LoadModel.py
+++ machine_learning/LoadModel.py
@@ -1,9 +1,32 @@
 import torch
+from torch.utils import data
+import torchvision
 import SetupModel as sm
+from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Load Model
 
 device = sm.get_default_device()
 model = sm.MnistModel(sm.input_size, sm.hidden_size, sm.num_classes)
-model.load_state_dict(torch.load('Model.pt'))
-model.to(device)
+model.load_state_dict(torch.load('machine_learning/Model.pt',map_location=device))
+# Remove map_location above and uncomment below for GPU loading
+# model.to(device)
+
+
+"""FOR TESTING"""
+
+# dataset = MNIST(root='machine_learning/data/',train = False,transform=transforms.ToTensor())
+
+# def displayImage(img,model):
+#     xb = img.unsqueeze(0)
+#     yb = model(xb)
+#     _, preds = torch.max(yb,dim=1)
+#     return preds[0].item()
+
+# img,label = dataset[0]
+# plt.imshow(img[0],cmap='gray')
+# plt.suptitle(f'Prediction: {displayImage(img,model)}')
+# plt.show()
