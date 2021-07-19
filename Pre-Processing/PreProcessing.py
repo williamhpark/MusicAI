07e5b8f9d6d582e1f45effa48994c5733670eff4
TobiasB4 <Tobi.Bobb.TB@gmail.com>
2021-07-19 10:13:20 -0700

    Some unfinished pre-processing functions

---
+++ Pre-Processing/PreProcessing.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import numpy as np
+
+
+def rescaleImage(img, scale=0.75):
+    """Rescale image"""
+    width = int(img.shape[1]*scale)
+    height = int(img.shape[0]*scale)
+
+    dimensions = (width, height)
+    return cv.resize(img, dimensions, interpolation=cv.INTER_AREA)
+
+
+def grayscaleImage(img_Path):
+    """Apply grayscale onto image and then rescale"""
+    img = cv.imread(str(img_Path), cv.IMREAD_GRAYSCALE)
+    img = rescaleImage(img, scale=0.4)
+
+    # Show image if wanted
+    # cv.imshow('Grayscale Resized Image', img)
+    # cv.waitKey(0)
+    return img
+
+
+def blurImage(img):
+    """Apply Gaussian blurring on image"""
+    out = cv.GaussianBlur(img, (9, 9), 0,)
+    cv.imshow('Gaussian Blur', out)
+    cv.waitKey(0)
+    return out
+
+
+def thresholdImage(img):
+    """Adaptive Gaussian Thresholding"""
+    out = cv.adaptiveThreshold(
+        img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    cv.imshow('Gaussian Thresholding', out)
+    cv.waitKey(0)
+    return out
+
+
+def invertImage(img):
+    """Invert and dilate image"""
+    invert = cv.bitwise_not(img, img)
+    kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]], np.uint8)
+    out = cv.dilate(invert, kernel)
+    cv.imshow('Inverted and Dilated Image', out)
+    cv.waitKey(0)
+    return out
